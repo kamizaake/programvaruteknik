d20e659fcbfd1d27b9e697779027c5886fa8df18
Åke Wallin <awi@hig.se>
2015-03-02 10:03:06 +0100

    Added loop

--- GitTest/src/GitTestClass.java
+++ GitTest/src/GitTestClass.java
@@ -3,6 +3,9 @@ public class GitTestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hej");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hej world");
+		}
 
 	}
 
